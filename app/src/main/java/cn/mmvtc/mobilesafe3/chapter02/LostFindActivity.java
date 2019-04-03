7a5aa55fda8669696ce5d9fb043af3e4494badd0
31702160326 <947064269@qq.com>
2019-04-03 20:37:53 +0800

    test

---
+++ app/src/main/java/cn/mmvtc/mobilesafe3/chapter02/LostFindActivity.java
@@ -0,0 +1,23 @@
+package cn.mmvtc.mobilesafe3.chapter02;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.CompoundButton;
+
+import cn.mmvtc.mobilesafe3.R;
+
+public class LostFindActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setup1);
+	}
+
+
+}
---
+++ app/src/main/java/cn/mmvtc/mobilesafe3/chapter02/entity/ContactInfo.java
@@ -0,0 +1,8 @@
+package cn.mmvtc.mobilesafe3.chapter02.entity;
+
+public class ContactInfo {
+	
+	public String id;
+	public String name;
+	public String phone;
+}
